9549ad5f0e031aef49bc9513c5a60705fa8436b4
IbrahimKalemci <githubibo@gmail.com>
2023-06-12 14:41:32 +0300

    Mini Website Projeleri
    4 adet ödev tarzı pür Html CSS JS ile Yapılmış Responsive Site Tasarımları

---
+++ Websites/simpleweb/index.js
@@ -0,0 +1,9 @@
+
+const themeToggler = document.querySelector(".theme-toggler");
+
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables')
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+})
